41b6b69a2fcfc5f52a3c29f39c1c3eac33ec3577
Adam Farmelo <afarmelo@asu.edu>
2018-11-15 12:00:14 -0500

    Added singleton pattern to Apiary

--- src/main/java/singleton/Apiary.java
+++ src/main/java/singleton/Apiary.java
@@ -1,5 +1,22 @@
 package main.java.singleton;
 
+/**
+ * The Apiary class is a Singleton in the Beehive simulator. 
+ * Apiaries have beehives, which have rooms, which spawn bees.
+ * @author Adam Farmelo
+ * @version 0.1
+ *
+ */
 public class Apiary {
 
+    private Apiary _instance;
+    
+    private Apiary() {}
+    
+    public Apiary getInstance() {
+        if (_instance == null) {
+            _instance = new Apiary();
+        }
+        return _instance;
+    }
 }
