2231e4169a0dea363abef7274eca363a5668a7a0
Adam Farmelo <afarmelo@asu.edu>
2018-11-15 11:46:18 -0500

    Added ApiaryMain to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
 package main.java;
-
+import main.java.singleton.ApiaryMain;
+/**
+ * Here is the entry point into the Design Patterns assignment.
+ * 
+ * @author Adam Farmelo
+ */
 public class Main {
 
+    /**
+     * main method runs all mains from the other packages
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello from Main!");
+        ApiaryMain.main(args);
     }
 }
