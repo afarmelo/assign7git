641cd6c3afbeca6df2db4f6dfd46b7fa1719648b
Adam Farmelo <afarmelo@asu.edu>
2018-11-15 11:45:47 -0500

    Added Apiary class and ApiaryMain (Singleton)

---
+++ src/main/java/singleton/Apiary.java
@@ -0,0 +1,5 @@
+package main.java.singleton;
+
+public class Apiary {
+
+}
---
+++ src/main/java/singleton/ApiaryMain.java
@@ -0,0 +1,17 @@
+package main.java.singleton;
+/**
+ * ApiaryMain is the entry point into the Apiary class, Singleton pattern
+ * @author Adam Farmelo
+ *
+ */
+public class ApiaryMain {
+
+    /**
+     * main method for Apiary class
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello from Apiary!");
+    }
+
+}
