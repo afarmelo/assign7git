304d79e508ea1c88a41e0f54c5c79065739d14b4
Adam Farmelo <afarmelo@asu.edu>
2018-11-15 11:59:18 -0500

    Created BeeSpecies and Bee classes

---
+++ src/main/java/Bee.java
@@ -0,0 +1,17 @@
+package main.java;
+
+/**
+ * Bees live in beehives, and are of a specific 'species' 
+ * which grants them bonuses doing certain things.
+ * @author Adam Farmelo
+ * @version 0.1
+ *
+ */
+public class Bee {
+
+    private BeeSpecies species;
+    
+    public Bee() {
+        
+    }
+}
---
+++ src/main/java/BeeSpecies.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * BeeSpecies represents any of the different species of Bees 
+ * which could live in a beehive.
+ * @author Adam Farmelo
+ * @version 0.1
+ *
+ */
+public enum BeeSpecies {
+    WARRIOR, FARMER, TRADER, ROBOT
+}
