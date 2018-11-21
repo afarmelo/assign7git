db367c7b679b6e0a74dae7a17338bdcc1ba29041
Adam Farmelo <afarmelo@asu.edu>
2018-11-21 06:41:12 -0500

    Beehive director implemented

--- src/main/java/builder/BeehiveDirector.java
+++ src/main/java/builder/BeehiveDirector.java
@@ -1,8 +1,19 @@
 package main.java.builder;
 
+/**
+ * The BeehiveDirector is the class which does the building. For the Builder
+ * pattern, the abstract builder provides the methods for the children
+ * to inherit and override, then the director is what gets invoked to 
+ * do the building.
+ * @author Adam Farmelo
+ * @version 0.1
+ *
+ */
 public class BeehiveDirector {
 
     public void buildHive(BeehiveBuilder bhBuilder) {
-        
+        bhBuilder.AddRooms();
+        bhBuilder.AddBees();
+        bhBuilder.AddQueen();
     }
 }
