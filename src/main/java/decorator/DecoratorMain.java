53b704e8654ffe3e5e1151009e9eab106d590b52
Adam Farmelo <afarmelo@asu.edu>
2018-11-21 07:24:53 -0500

    Created entry point for decorators

---
+++ src/main/java/decorator/DecoratorMain.java
@@ -0,0 +1,11 @@
+package main.java.decorator;
+
+public class DecoratorMain {
+
+    public static void main(String[] args) {
+        // Create some bees
+        // Create some decorators
+        // add the bees to the decorators
+        System.out.println("Hello from Decorator!");
+    }
+}
