106461dc1d2dc0c7d604cfc77e9f95adcf417e08
Adam Farmelo <afarmelo@asu.edu>
2018-11-21 06:41:34 -0500

    Farmer Bee Builder implemented

--- src/main/java/builder/FarmerBeehiveBuilder.java
+++ src/main/java/builder/FarmerBeehiveBuilder.java
@@ -1,5 +1,35 @@
 package main.java.builder;
 
+import main.java.BeeSpecies;
+import main.java.Beehive;
+
+/**
+ * The farmer bees have mastered the art of producing their own resources,
+ * as such they understand the intricacies of growing and harvesting. All
+ * the farmer bee resource harvesting speeds are increased 20%. Because
+ * they are adept at providing food for their colonies, there is an increased
+ * prevalence of spawning and sometimes an overabundance of Bees.
+ * @author Adam Farmelo
+ * @version 0.1
+ *
+ */
 public class FarmerBeehiveBuilder extends BeehiveBuilder {
+
+    private Beehive _hive = new Beehive();
+    
+    @Override
+    public void AddRooms() {
+        _hive.addRooms(50, 8);
+    }
+
+    @Override
+    public void AddBees() {
+        _hive.addBees(30, BeeSpecies.FARMER);
+    }
+
+    @Override
+    public void AddQueen() {
+        _hive.addQueen(BeeSpecies.FARMER);
+    }
 
 }
