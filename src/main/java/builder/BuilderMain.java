34184d51e833af90f7dd9b6710d255c9572e5dd2
Adam Farmelo <afarmelo@asu.edu>
2018-11-21 06:42:57 -0500

    Builder main method started

---
+++ src/main/java/builder/BuilderMain.java
@@ -0,0 +1,12 @@
+package main.java.builder;
+/**
+ * BuilderMain is the entry point into the Beehive class, builder pattern
+ * @author Adam Farmelo
+ *
+ */
+public class BuilderMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Beehive!");
+    }
+}
