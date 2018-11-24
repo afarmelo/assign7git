c1201342dc8887cc79433c0d6381778e235758ea
Adam Farmelo <afarmelo@asu.edu>
2018-11-24 11:46:16 -0500

    Implemented prototype main

--- src/main/java/prototype/PrototypeMain.java
+++ src/main/java/prototype/PrototypeMain.java
@@ -1,8 +1,29 @@
 package main.java.prototype;
 
+import main.java.decorator.FastHarvest;
+import main.java.decorator.Haggle;
+
 public class PrototypeMain {
 
     public static void main() {
-       System.out.println("Hello from Prototype!");
+        System.out.println("\nPrototypeMain running...");
+        System.out.println("Creating a Farmer Bee");
+        
+        Farmer joe = new Farmer();
+        System.out.println("Joe:\n" + joe.toString());        
+        System.out.println("Cloning Joe...");
+        
+        Farmer john = (Farmer)joe.clone();
+        System.out.println("John:\n" + john.toString());
+        
+        System.out.println("Adding a decorator to Joe..");
+        FastHarvest farmerJoe = new FastHarvest(joe);
+        System.out.println("Farmer Joe:\n" + farmerJoe.toString());
+        
+        System.out.println("Adding a decorator to John..");
+        Haggle smoothTalkingJohn = new Haggle(john);
+        System.out.println("Smooth Talking John:\n" + smoothTalkingJohn.toString());
+        
+        System.out.println("...PrototypeMain finished.\n");
     }
 }
