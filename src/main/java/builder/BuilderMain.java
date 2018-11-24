30d5e04461cac0ebc4d9a4b5283be6e25e7191c7
Adam Farmelo <afarmelo@asu.edu>
2018-11-24 10:30:38 -0500

    Format fixes

--- src/main/java/builder/BuilderMain.java
+++ src/main/java/builder/BuilderMain.java
@@ -1,4 +1,7 @@
 package main.java.builder;
+
+import main.java.Beehive;
+
 /**
  * BuilderMain is the entry point into the builder pattern
  * @author Adam Farmelo
@@ -6,7 +9,25 @@ package main.java.builder;
  */
 public class BuilderMain {
 
-    public static void main() {
-        System.out.println("Hello from Beehive!");
+    public static void main(String[] args) {
+        System.out.println("\nBuilderMain running...");
+        
+        System.out.println("Creating a director..");
+        BeehiveDirector dir = new BeehiveDirector();
+        
+        System.out.println("Creating beehive builders..");
+        FarmerBeehiveBuilder farmBuilder = new FarmerBeehiveBuilder();
+        TraderBeehiveBuilder tradeBuilder = new TraderBeehiveBuilder();
+        
+        System.out.println("Building hives..\n");
+        dir.buildHive(farmBuilder);
+        Beehive farmHive = (Beehive)farmBuilder.getHive();
+        System.out.println(farmHive.toString());
+        
+        dir.buildHive(tradeBuilder);
+        Beehive tradeHive = (Beehive)tradeBuilder.getHive();
+        System.out.println(tradeHive.toString()); 
+        
+        System.out.println("...BuilderMain finished.\n");
     }
 }
